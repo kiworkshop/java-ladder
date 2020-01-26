93fe5e82cd6444e25311e065db7cb75895556916
taesikyoo <yrns1213@naver.com>
2020-01-26 22:30:04 +0900

    refactor: InputScanner의 코드를 InputView로 추출

---
+++ src/main/java/laddergame/view/InputView.java
@@ -0,0 +1,20 @@
+package laddergame.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String MESSAGE_GET_PLAYER_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String MESSAGE_GET_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+
+    public String getPlayerNamesFromConsole() {
+        System.out.println(MESSAGE_GET_PLAYER_NAMES);
+        return SCANNER.nextLine();
+    }
+
+    public int getLadderHeightFromConsole() {
+        System.out.println(MESSAGE_GET_LADDER_HEIGHT);
+        return SCANNER.nextInt();
+    }
+}
