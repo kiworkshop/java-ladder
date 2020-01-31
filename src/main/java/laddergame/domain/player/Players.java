a4364770e511a5b0340531e755ee5f64ab43fb75
taesikyoo <yrns1213@naver.com>
2020-01-31 16:03:57 +0900

    refactor : OutputView의 일부 메서드를 Players로 이주
    - Outputview의 printResult 메서드 내의 추상화 수준을 맞추기 위해
    printResultMessage 메서드로 코드를 분리
    - Player의 이름을 출력을 위해 align하는 로직을 Players로 추출
    - 추출한 메서드인 getAlignedPlayerNames 내의 로직을 정리하여 3개의
    메서드로 쪼갬

--- src/main/java/laddergame/domain/player/Players.java
+++ src/main/java/laddergame/domain/player/Players.java
@@ -1,13 +1,15 @@
 package laddergame.domain.player;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public class Players {
 
-    private final List<Player> players;
+    private List<Player> players;
 
     public Players(final List<Player> players) {
         if (hasDuplicatedNames(players)) {
@@ -26,7 +28,41 @@ public class Players {
         return players.size();
     }
 
-    public Stream<Player> stream() {
-        return players.stream();
+    public String getAlignedPlayerNames() {
+        List<String> playerNames = playerNamesToList();
+
+        List<String> alignedPlayerNames = alignPlayerNames(playerNames);
+
+        return playerNamesToString(alignedPlayerNames);
+    }
+
+    private List<String> playerNamesToList() {
+        List<String> playerNames = new ArrayList<>();
+
+        players.forEach(player -> playerNames.add(player.getName()));
+        return playerNames;
+    }
+
+    private List<String> alignPlayerNames(List<String> playerNames) {
+        List<String> alignedPlayerNames = new ArrayList<>();
+
+        for (String playerName : playerNames) {
+            String alignedPlayerName = alignPlayerName(playerName);
+            alignedPlayerNames.add(alignedPlayerName);
+        }
+        return alignedPlayerNames;
+    }
+
+    private String alignPlayerName(String playerName) {
+        return StringUtils.leftPad(playerName, Player.PLAYER_NAME_MAX_LENGTH + 1);
+    }
+
+    private String playerNamesToString(List<String> playerNames) {
+        String alignedPlayerNames ="";
+
+        for (String playerName : playerNames) {
+            alignedPlayerNames += playerName;
+        }
+        return alignedPlayerNames;
     }
 }
