6e8f9873ac178120b3fcbfe0ea6f02a53a03031e
jaeju.jang <jjj0611@gmail.com>
2020-02-09 00:10:21 +0900

    ladder 클래스의 toString을 구현하여 사다리 출력 로직을 변경했습니다.

--- src/main/java/domain/ladder/Ladder.java
+++ src/main/java/domain/ladder/Ladder.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Ladder {
 
@@ -56,15 +57,46 @@ public class Ladder {
         return new LadderResult(ladderResult);
     }
 
-    public List<User> getUsers() {
-        return users;
+    @Override
+    public String toString() {
+        return getUsersString() + "\n" +
+                getRowsString() +
+                getResultsString();
     }
 
-    public List<Row> getRows() {
-        return rows;
+    private String getUsersString() {
+        List<String> userNames = users.stream().map(User::getFormattedName).collect(Collectors.toList());
+        return String.join(" ", userNames);
     }
 
-    public List<Result> getResults() {
-        return results;
+    private String getResultsString() {
+        List<String> resultList = results.stream().map(Result::getFormattedResult).collect(Collectors.toList());
+        return String.join(" ", resultList);
+    }
+
+    public String getRowsString() {
+        StringBuilder sb = new StringBuilder();
+        for (Row row : rows) {
+            sb.append(getRowString(row) + "\n");
+        }
+        return sb.toString();
+    }
+
+    private String getRowString(Row row) {
+        List<Step> steps = row.getSteps();
+        StringBuilder sb = new StringBuilder();
+        sb.append("  |");
+        for (Step step : steps) {
+            sb.append(getStepString(step));
+            sb.append("|");
+        }
+        return sb.toString();
+    }
+
+    private String getStepString(Step step) {
+        if (step.exist()) {
+            return "-----";
+        }
+        return "     ";
     }
 }
--- src/main/java/view/output/GameOutputView.java
+++ src/main/java/view/output/GameOutputView.java
@@ -1,62 +1,17 @@
 package view.output;
 
 import domain.ladder.Ladder;
-import domain.ladder.Row;
-import domain.ladder.Step;
 import domain.result.LadderResult;
 import domain.result.Result;
-import domain.user.User;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class GameOutputView {
 
     public static void printLadder(Ladder ladder) {
-        printUsers(ladder.getUsers());
-        printRows(ladder.getRows());
-        printResults(ladder.getResults());
-    }
-
-    public static void printUsers(List<User> users) {
-        List<String> userNames = users.stream().map(User::getFormattedName).collect(Collectors.toList());
-        String usersOnLadder = String.join(" ", userNames);
-        System.out.println(usersOnLadder);
-
-    }
-
-    public static void printResults(List<Result> results) {
-        List<String> resultList = results.stream().map(Result::getFormattedResult).collect(Collectors.toList());
-        String resultsUnderLadder = String.join(" ", resultList);
-        System.out.println(resultsUnderLadder);
+        System.out.println(ladder);
     }
 
     public static void printLadderResult(LadderResult ladderResult) {
         System.out.println(ladderResult);
-    }
-
-    public static void printRows(List<Row> rows) {
-        for (Row row : rows) {
-            printRow(row);
-        }
-    }
-
-    private static void printRow(Row row) {
-        List<Step> steps = row.getSteps();
-        StringBuilder sb = new StringBuilder();
-        sb.append("  |");
-        for (Step step : steps) {
-            sb.append(getStepString(step));
-            sb.append("|");
-        }
-        System.out.println(sb.toString());
-    }
-
-    private static String getStepString(Step step) {
-        if (step.exist()) {
-            return "-----";
-        }
-        return "     ";
     }
 
     public static void printResult(Result result) {
